6d17ee6f244598fb702d34bb9ada35b751e74b6d
SzMartin1 <78854567+SzMartin1@users.noreply.github.com>
2021-03-12 18:42:54 +0100

    Add files via upload

---
+++ Konyv/Konyv/Konyv.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konyv
+{
+    class Konyv
+    {
+        private string cim;
+        private string szerzo;
+        private int megjelenesEve;
+        private int ar;
+
+        public Konyv(string cim, string szerzo)
+        {
+            this.cim = cim;
+            this.szerzo = szerzo;
+        }
+
+        public Konyv(string cim, string szerzo, int megjelenesEve, int ar)
+        {
+            this.cim = cim;
+            this.szerzo = szerzo;
+            this.megjelenesEve = megjelenesEve;
+            this.ar = ar;
+        }
+
+        public int KonyvAr
+        {
+            get { return ar; }
+            set { ar = value; }
+        }
+
+
+        public int MegjelenesEve
+        {
+            get { return megjelenesEve; }
+            set { megjelenesEve = value; }
+        }
+
+
+        public string KonyvSzerzo
+        {
+            get { return szerzo; }
+            set { szerzo = value; }
+        }
+
+
+        public string KonyvCim
+        {
+            get { return cim; }
+            set { cim = value; }
+        }
+
+        public override string ToString()
+        {
+            return szerzo + ": " + cim + "  Megjelenés éve: " + megjelenesEve + "  Ár: " + ar;
+        }
+
+        public void AratMegnovel(int szazalek)
+        {
+            ar += (int)(ar * szazalek / 100.0);
+        }
+
+        public string InformaciotKiir()
+        {
+            return szerzo + ": " + cim + "  Megjelenes eve: " + megjelenesEve + "  Ar: " + ar;
+        }
+    }
+}
---
+++ Konyv/Konyv/KonyvFuttathato.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Konyv
+{
+    class KonyvFuttathato
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDate = DateTime.Now;
+            int year = currentDate.Year;
+            Konyv konyv = new Konyv("The song of ice and fire", "George R.R. Martin",year,2500);
+            
+
+            Console.WriteLine(konyv.ToString());
+
+            konyv.AratMegnovel(25);
+
+            Console.WriteLine(konyv.ToString());
+        }
+    }
+}
